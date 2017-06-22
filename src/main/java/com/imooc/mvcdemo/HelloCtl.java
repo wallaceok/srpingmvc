89ba7699a0b96237da1e2de45b79d362fc8971e2
lihaimin126@126.com <wallaceok>
2017-06-23 07:31:54 +0800

    springMVC

---
+++ src/main/java/com/imooc/mvcdemo/HelloCtl.java
@@ -0,0 +1,17 @@
+package com.imooc.mvcdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloCtl {
+	
+	@RequestMapping(value="/hello")  
+    public String HelloWorld(Model model){  
+        model.addAttribute("message","Hello World!!!");  
+        return "home";
+	}
+	
+}
